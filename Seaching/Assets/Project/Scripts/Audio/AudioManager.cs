526538741599df4e6d93a0cffea2bd1c7add15df
ei2000 <ei12431243@gmail.com>
2025-12-01 17:29:07 +0900

    音の導入
    シーンを跨いでも同じAudioManagerscriptを使って音を出せます。

---
+++ Seaching/Assets/Project/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    [SerializeField] private AudioSource bgmSource;
+    [SerializeField] private AudioSource seSource;
+
+    public void PlayBGM(AudioClip clip)
+    {
+        bgmSource.clip = clip;
+        bgmSource.loop = true;
+        bgmSource.Play();
+    }
+
+    public void PlaySE(AudioClip clip)
+    {
+        seSource.PlayOneShot(clip);
+    }   
+
+    public void StopBGM()
+    {
+        bgmSource.Stop();
+    }
+
+        public void StopSE()
+    {
+        seSource.Stop();
+    }
+}
