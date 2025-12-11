9aa7c6518128c8cb0c7372a25fd76c16d67e178c
ei2000 <ei12431243@gmail.com>
2025-12-11 17:50:22 +0900

    スコアを参照
    スコアを参照できるようにしました。

--- Seaching/Assets/Project/Scripts/score/ScoreManager.cs
+++ Seaching/Assets/Project/Scripts/score/ScoreManager.cs
@@ -1,31 +1,54 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    [SerializeField] Text scoreText;
-    private int score = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
+    public static ScoreManager Instance;
+    private int score = 100000;
 
-    private void OnEnable()
+    private void Awake()
+    {
+        if (Instance == null)//シーンを跨いでも値を破棄しない
+        {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()//score加算
     {
         BreakableObject.OnObjectBroken += AddScore;
     }
 
-        private void OnDisable()
+        private void OnDisable()//score減少
     {
         BreakableObject.OnObjectBroken -= AddScore;
     }
 
-    private void AddScore(int value)
+    private void AddScore(int value)//加算したスコアを表示する
     {
         score += value;
         scoreText.text = "Score:"+score;
         Debug.Log("Score:"+ score);
     }
 
+    public int GetScore()
+    {
+        return score;
+    }
+
+
     void Start()
     {
-        
+
     }
     void Update()
     {
